ee6329b0891e0a57d077fe6eace23b98290d0453
EucheriaK <ekureva@gmail.com>
2022-11-21 17:19:32 +0200

    integers

--- main.h
+++ main.h
@@ -12,7 +12,9 @@ char c;
 void(*func)(va_list;
 }
 
+int _printf(const char *format, ...)
 int _putchar(char c);
 int print_int(va_list args, char *buffer)
+	int flags, int width, int precision, int size)
 
 #endif
