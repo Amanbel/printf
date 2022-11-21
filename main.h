58291e88eff90f56a1076e4db5f72960bb22c218
EucheriaK <ekureva@gmail.com>
2022-11-21 13:07:32 +0200

    integers

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct pnt
+{
+char c;
+void(*func)(va_list;
+}
+
+int _putchar(char c);
+
+#endif
