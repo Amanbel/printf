914060382693b82a92f0cd2cbb3093938500e410
Amanbel <amanuel11belachew@gmail.com>
2022-11-22 11:06:00 -0800

    integers

--- main.h
+++ main.h
@@ -1,20 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <limits.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+int _printf(const char *format, ...);
+void print_string(va_list args);
+void print_char(va_list args);
+void print_float(va_list args);
+void print_int(va_list args);
+int _putchar(char c);
+int print_int(va_list args, char *buffer);
+int flags, int width, int precision, int size
+
+/**
+ * struct pnt - a structure holding a character and a pointer to function
+ * @c: charcter
+ * @func: pointer to function
+ */
 
 typedef struct pnt
 {
-char c;
-void(*func)(va_list;
-}
-
-int _printf(const char *format, ...)
-int _putchar(char c);
-int print_int(va_list args, char *buffer)
-	int flags, int width, int precision, int size)
+	char c;
+	void (*func)(va_list);
+} pnt;
 
 #endif
