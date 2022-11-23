6a308ce36013a2b54c96d3f55b0fda0e0b68d8aa
Amanbel <amanuel11belachew@gmail.com>
2022-11-23 00:01:51 -0800

    main_printf_func

--- print_f.c
+++ print_f.c
@@ -1,51 +1,51 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * _printf - duplicate of the standard printf function
+ * _printf - duplicate printf function
  * @format: input string
- * Return: 0
+ * Return: chars
  */
 
 int _printf(const char *format, ...)
 {
-	int i, key;
-	const char *ptr;
-
-	pnt test_c[2] = {{'c', print_char}, {'s', print_string}};
-
+	int i, prntd = 0, prntd_ch = 0;
+	int flags, width, precision, size, buff = 0;
 	va_list args;
+	char buffer[BUFF_SIZE];
 
-	int len = strlen(format);
-
-	ptr = format;
-	key = 0;
+	if (format == NULL)
+		return (-1);
 
 	va_start(args, format);
 
-	if (format != NULL)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		for (i = 0; i < len; i++)
+		if (format[i] != '%')
 		{
-			if (ptr[i] == '%')
-			{
-				i++;
-				while (key < 2)
-				{
-					if (test_c[key].c == ptr[i])
-					{
-						test_c[key].func(args);
-					}
-					key++;
-				}
-				key = 0;
-				continue;
-			}
-			_putchar(ptr[i]);
+			buffer[buff++] = format[i];
+			if (buff == BUFF_SIZE)
+				print_buffer(buffer, &buff);
+			prntd_ch++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i);
+			precision = get_precision(format, &i, args);
+			size = get_size(format, &i);
+			++i;
+			prntd = handle_print(format, &i, args, buffer,
+					flags, width, precision, size);
+			if (prntd == -1)
+				return (-1);
+			prntd_ch += prntd;
 		}
 	}
+
+	print_buffer(buffer, &buff);
+
 	va_end(args);
-	return (0);
+
+	return (prntd_ch);
 }
